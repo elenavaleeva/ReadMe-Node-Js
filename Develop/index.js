c516f28a0e2cd399bc8f6f61030280a66bfce82a
ELENA VALEEVA <elenavaleeva05@gmail.com>
2023-01-28 01:06:38 -0500

    final touch

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,102 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown')
+
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for information about my application repository
+// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+// WHEN I enter my project title
+
+// fs.readFile( path.join(__dirname,'Develop', 'units', 'generateMarkdown.js'), 'utf8',(err, data)  => {
+//     if (err) throw err;
+//     console.log(data);
+// })
+
+// // process.on('uncaughtException', err => {
+// //     console.error(`There was an uncaught error: ${err}`);
+// //     process.exit(1);
+// // })
+
+// fs.writeFile( path.join(__dirname,'Develop', 'units', 'README.md'),' Nice to meet you', (err)  => {
+//     if (err) throw err;
+//     console.log(`write complete`);
+// })
+
+// fs.appendFile( path.join(__dirname,'Develop', 'units', 'README.md'),' Text check', (err)  => {
+//     if (err) throw err;
+//     console.log(`append complete`);
+// })
+console.log(`Hello...`);
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [ 
+    {
+      type: 'input',
+      message: 'What is your user project title ?',
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: 'What is your entitled Description?',
+      name: 'description',
+    },
+    {
+      type: 'input',
+      message: 'What is your Installation',
+      name: 'Installation',
+    },
+    {
+      type: 'input',
+      message: 'What is your usage?',
+      name: 'usage',
+    },
+    {
+      type: 'list',
+      message: 'What is your License?',
+      name: 'license',
+      choices: ['MIT', 'APACHE', 'IBM']
+    },
+    {
+        type: 'input',
+        message: 'What is your contributing?',
+        name: 'contributing',
+      },
+      {
+        type: 'input',
+        message: 'What is your GitHub?',
+        name: 'GitHub',
+      },
+      {
+        type: 'input',
+        message: 'What is your email?',
+        name: 'email',
+      },
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+ ]
+ inquirer.prompt(questions) 
+  .then((response) => {
+    response.confirm === response.password
+      ? console.log('Success!')
+      : console.log('Success!')
+      fs.writeFileSync('README.md', generateMarkdown(response))
+  }
+)
+ 
+
+
+
+//  // TODO: Create a function to write README file
+//  fs.writeFile('log.txt', process.data, (err) =>
+//  err ? console.error(err) : console.log('Success!')
+// );
+
 
 // TODO: Create a function to initialize app
 function init() {}
 
 // Function call to initialize app
 init();
+
+
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,10 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+if (license === "APACHE") {
+  return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+}
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -13,6 +17,14 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+## description
+${data.description}
+## Installation
+${data.Installation}
+## usage
+${data.usage}
+## license
+${renderLicenseBadge(data.license)}
 
 `;
 }
