ab094981f98c1deed8c60f9f08341ca19a5e7137
ELENA VALEEVA <elenavaleeva05@gmail.com>
2023-01-28 01:30:02 -0500

    getting data

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -8,11 +8,19 @@ if (license === "APACHE") {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+if (license === "MIT") {
+  return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+}
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === "IBM") {
+    return " [![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)"
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -25,7 +33,12 @@ ${data.Installation}
 ${data.usage}
 ## license
 ${renderLicenseBadge(data.license)}
-
+## contributing
+${data.contributing}
+## GitHub
+${data.GitHub}
+## email
+${data.email}
 `;
 }
 
